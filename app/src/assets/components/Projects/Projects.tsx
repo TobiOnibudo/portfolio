ffba8c5b8204490c7fc996f85fb467b6308efb23
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-12-29 23:03:04 -0400

    Completed responsiveness

--- app/src/assets/components/Projects/Projects.tsx
+++ app/src/assets/components/Projects/Projects.tsx
@@ -10,30 +10,36 @@ function Projects()
             <h3 className="portfolioTitle"> My Personal Projects</h3>
 
             <div className="portfolio">
-            <Project 
-                Title="Personal Library" 
-                description="This is a personal book library made for
-                users to keep track of books they are interested in and have read. This would be really
-                useful for fellow avid readers as it can be quite hard to keep track of hardcopy books 
-                in their library." image={book} link = {"https://github.com/TobiOnibudo/Book"}
-                stack={[
-                    { id : 0 ,reactIcon:<FaHtml5  className="stackIcon"/>},
-                    {id:1,reactIcon:<FaHtml5  className="stackIcon"/>}
-                ]} 
-            />
-
-            <Project 
-                Title="Personal Library" 
-                description="This is a personal book library made for
-                users to keep track of books they are interested in and have read. This would be really
-                useful for fellow avid readers as it can be quite hard to keep track of hardcopy books 
-                in their library." image={book} link = {"https://github.com/TobiOnibudo/Book"}
-                stack={[
-                    { id : 0 ,reactIcon:<FaHtml5  className="stackIcon"/>},
-                    {id:1,reactIcon:<FaHtml5  className="stackIcon"/>}
-                ]} 
-            />
+            
+            <div className="projectCard" data-scroll>
+                <Project 
+                    Title="Personal Library" 
+                    description="This is a personal book library made for
+                    users to keep track of books they are interested in and have read. This would be really
+                    useful for fellow avid readers as it can be quite hard to keep track of hardcopy books 
+                    in their library." image={book} link = {"https://github.com/TobiOnibudo/Book"}
+                    stack={[
+                        { id : 0 ,reactIcon:<FaHtml5  className="stackIcon"/>},
+                        {id:1,reactIcon:<FaHtml5  className="stackIcon"/>}
+                    ]} 
+                />
             </div>
+
+            <div className="projectCard">
+                <Project 
+                    Title="Personal Library" 
+                    description="This is a personal book library made for
+                    users to keep track of books they are interested in and have read. This would be really
+                    useful for fellow avid readers as it can be quite hard to keep track of hardcopy books 
+                    in their library." image={book} link = {"https://github.com/TobiOnibudo/Book"}
+                    stack={[
+                        { id : 0 ,reactIcon:<FaHtml5  className="stackIcon"/>},
+                        {id:1,reactIcon:<FaHtml5  className="stackIcon"/>}
+                    ]} 
+                />
+            </div>
+            </div>
+            
         </div>
     )
 }
