23c70b264907216f31aa07e65694796b4dadcd3b
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-12-29 20:07:23 -0400

    Completed structure for Project Component

--- app/src/assets/components/Projects/Project.tsx
+++ app/src/assets/components/Projects/Project.tsx
@@ -1,19 +1,39 @@
 import "../../../Styles/Project.css"
 
+type icon = 
+{
+    id : Number,
+    reactIcon : React.ReactNode 
+}
+
 type ProjectProps = {
     Title : String;
     description : String;
     image: string;
+    stack : Array<icon>;
+    link : string
 }
 
 
-function Project(props : ProjectProps){
+
+function Project ({
+    Title ,
+    description ,
+    image,
+    stack,
+    link
+} : ProjectProps) {
   return (
     <div>
-        <img src={props.image} alt="Image representing the project" className="projImg" />
-        <p className="projTitle"> {props.Title} </p>
-        <p className="projDesc">{props.description}</p>
-        <button className="projRepo">View</button>
+        <img src={image} alt="Image representing the project" className="projImg" />
+        <h4 className="projTitle"> {Title} </h4>
+        <p className="projDesc">{description}</p>
+        
+        <div>{stack.map((item)=> 
+        {
+            return <div key= {item.id.toString()} >{item.reactIcon} </div>
+        })}</div>
+        <a href={link} target="_blank"> <button className="projRepo">View</button> </a>
     </div>
   )
 };
--- app/src/assets/components/Projects/Projects.tsx
+++ app/src/assets/components/Projects/Projects.tsx
@@ -1,9 +1,37 @@
 import "../../../Styles/Projects.css"
+import Project from "./Project";
+import book from "../../../img/Book-project.jpg"
+import { FaHtml5 } from "react-icons/fa6";
+
 function Projects()
 {
     return (
-        <div>
+        <div className="portfolio">
          Projects 
+
+            <Project 
+                Title="Personal Library" 
+                description="This is a personal book library made for
+                users to keep track of books they are interested in and have read. This would be really
+                useful for fellow avid readers as it can be quite hard to keep track of hardcopy books 
+                in their library." image={book} link = {"https://github.com/TobiOnibudo/Book"}
+                stack={[
+                    { id : 0 ,reactIcon:<FaHtml5  className="stackIcon"/>},
+                    {id:1,reactIcon:<FaHtml5  className="stackIcon"/>}
+                ]} 
+            />
+
+            <Project 
+                Title="Personal Library" 
+                description="This is a personal book library made for
+                users to keep track of books they are interested in and have read. This would be really
+                useful for fellow avid readers as it can be quite hard to keep track of hardcopy books 
+                in their library." image={book} link = {"https://github.com/TobiOnibudo/Book"}
+                stack={[
+                    { id : 0 ,reactIcon:<FaHtml5  className="stackIcon"/>},
+                    {id:1,reactIcon:<FaHtml5  className="stackIcon"/>}
+                ]} 
+            />
         </div>
     )
 }
