a3cff404737e43a6168e5fdb9a4149183205488c
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-12-30 22:44:46 -0400

    Made a few changes to test new build

--- src/assets/components/Contact.tsx
+++ src/assets/components/Contact.tsx
@@ -62,37 +62,38 @@ function Contact(){
   };
 
   return (
-    <section className="contact">
-     <h1 className="contactPageTitle">Contact Me</h1>
-      <span className="contactDesc">
-        Please fill out the form below to discuss
-        any work or collaboration opportunities.
-      </span>
+    
+      <section className="contact">
+      <h1 className="contactPageTitle">Contact Me</h1>
+        <span className="contactDesc">
+          Please fill out the form below to discuss
+          any work or collaboration opportunities.
+        </span>
 
-      <form className="contactForm"  onSubmit={handleSubmit(sendEmail)}>
-        <input type="text" className="name" placeholder="Your Name" {...register("your_name",{
-                        required: true,
-                        maxLength: 20,
-                        pattern: /^[A-Za-z]+$/i
-                    })} />
-        
-        <input type="email" className="email" placeholder="Your Email"  {...register("your_email", {
-                        required: true,
-                        pattern: /\S+@\S+\.\S+/
-                    })}/>
-        {errors.your_email?.message != "" && <p className="error" >{errors.your_email?.message}</p>}
-        
-        <textarea className="msg" rows={5} placeholder="Your message" {...register("message",{
-                        required: true,
-                        pattern: /^[A-Za-z]+$/i
-                    })} ></textarea>
-        {errors.message?.message != "" && <p className="error">{errors.message?.message}</p>}
-        
-        <button type="submit" value="Send" className="submitBtn">Submit</button>
-      </form>
+        <form className="contactForm"  onSubmit={handleSubmit(sendEmail)}>
+          <input type="text" className="name" placeholder="Your Name" {...register("your_name",{
+                          required: true,
+                          maxLength: 20,
+                          pattern: /^[A-Za-z]+$/i
+                      })} />
+          
+          <input type="email" className="email" placeholder="Your Email"  {...register("your_email", {
+                          required: true,
+                          pattern: /\S+@\S+\.\S+/
+                      })}/>
+          {errors.your_email?.message != "" && <p className="error" >{errors.your_email?.message}</p>}
+          
+          <textarea className="msg" rows={5} placeholder="Your message" {...register("message",{
+                          required: true,
+                          pattern: /^[A-Za-z]+$/i
+                      })} ></textarea>
+          {errors.message?.message != "" && <p className="error">{errors.message?.message}</p>}
+          
+          <button type="submit" value="Send" className="submitBtn">Submit</button>
+        </form>
 
-   
-    </section>
+    
+      </section>
   )
 };
 
