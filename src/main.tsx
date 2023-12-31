69b3567fad0e69f2d69e8afdbcb569d191f0b2df
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-12-30 22:53:33 -0400

    Switched Repo name

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename='/personal_portfolio'> 
+    <BrowserRouter basename='/portfolio'> 
       <App />
     </BrowserRouter>
   </React.StrictMode>
