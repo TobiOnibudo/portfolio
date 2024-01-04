a35ada0362d0a06c974bb8c3dec6eea747b4e95c
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-01-04 18:55:48 -0400

    Changed the email link in the footer

---
+++ src/assets/components/Footer/Footer.tsx
@@ -0,0 +1,29 @@
+import "./Footer.css"
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillMail } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import Resume from "../../../img/resume.png"
+import Pdf from "../../../img/Resume.pdf"
+
+function Footer()
+{
+
+    
+    return (
+        <footer className="footer">
+            <div className="links">
+                {/* linkedin */}
+                <a href="https://www.linkedin.com/in/tobi-onibudo/" target="_blank" > <AiFillLinkedin className="link"/> </a>
+                {/* github */}
+                <a href="https://github.com/TobiOnibudo" target="_blank" >  <AiFillGithub   className="link"/> </a>
+                {/* resume */}
+                    <a href={Pdf} target="_blank" ><img src={Resume} alt="" className="resume" /> </a>
+                {/* email */}
+                <a href="portfolio/Contact-me" target="_blank" > <AiFillMail className="link"/> </a>
+            </div>
+        </footer>
+    )
+}
+
+
+export default Footer
