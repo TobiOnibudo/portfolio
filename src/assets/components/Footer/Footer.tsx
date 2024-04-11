15626fb58995a4bd46d224d64e52e8485691a6e7
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-04-10 22:07:46 -0300

    Updating broken footer link

--- src/assets/components/Footer/Footer.tsx
+++ src/assets/components/Footer/Footer.tsx
@@ -4,6 +4,7 @@ import { AiFillMail } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
 import Resume from "../../../img/resume.png"
 import Pdf from "../../../img/Resume.pdf"
+import { NavLink } from "react-router-dom";
 
 function Footer()
 {
@@ -19,7 +20,7 @@ function Footer()
                 {/* resume */}
                     <a href={Pdf} target="_blank" ><img src={Resume} alt="" className="resume" /> </a>
                 {/* email */}
-                <a href="portfolio/Contact-me" target="_blank" > <AiFillMail className="link"/> </a>
+                <NavLink to="/Contact-me" target="_blank">  <AiFillMail className="link"/> </NavLink>
             </div>
         </footer>
     )
